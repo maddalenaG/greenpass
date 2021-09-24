3d0ab89dda8f90e732909b67fe6b2bc002e5996f
maddalena gammella <maddalena.gammella@stepsrl.org>
2021-09-24 10:52:52 +0200

    inizializzazione del progetto

---
+++ src/test/java/org/stepsrl/greenPass/GreenPassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stepsrl.greenPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
